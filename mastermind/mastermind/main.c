8ed905e7bbe26431e4162df85b7fac6eef299f5f
Alwin Rodewijk <alwin935@gmail.com>
2020-01-05 22:36:47 +0100

    Everything but the timer done.. probebly..

--- mastermind/mastermind/main.c
+++ mastermind/mastermind/main.c
@@ -81,13 +81,17 @@ void ReceiveString(char *str);
 void TransmitString(char *str);
 
 /* game variables */
+uint8_t showSecretCode = 1;		// 1 to show & 0 to hide
+
 uint8_t turn = 0;
 #define MAX_TURNS 12
+#define BUFFER_SIZE 30
 uint8_t lightLost = 0;
 uint8_t lightWon = 0;
 uint8_t lightCorrectNum[4] = {0, 0, 0, 0};
 uint8_t resetGame = 0;
 uint16_t countForRandomSeed = 0;
+char buffer[BUFFER_SIZE];
 enum gameState 
 {welcome, newGame, turnX, askInput, checkInput, showResults, won, lost, waiting}
 currentGameState;
@@ -98,37 +102,52 @@ void init_Random_Number(){
 }
 
 char generate_Random_Number(){
-	return (48 + ((rand() % 6) + 1));
+	char returnValue[2];
+	itoa(((rand() % 6) + 1), returnValue, 10);
+	return returnValue[0];
 }
 
-/* Setup Timer 0 with a frequency of 40 Hz */
+/* Setup Timer 0 with a frequency of 80 Hz */
 void init_Timer0()
 {
-	// - CTC mode of operation with OCR1A as TOP
+	// - CTC mode of operation with OCR0A as TOP
 	SetBit(TCCR0A, WGM01);
+	
+	// set prescaler to 1024
+	TCCR0B |= (1<<CS00) | (1<<CS01);// | (1<<CS02);
+	
+	// enable OCR0A on timer 0
+	TIMSK0 |= (1<<OCIE0A);
+	
+	// set output compare A to 249
+	OCR0A = 2;
+	
+	/*
+	
 
 	// set prescaler @ 1024
 	SetBit(TCCR0B, CS00);
 	SetBit(TCCR0B, CS01);
 	SetBit(TCCR0B, CS02);
 
+	// enable OCR0A on timer 0
+	SetBit(TIMSK0, OCIE0A);
 	//Set output compare value for channel A
-	OCR0A = 194;		//40 = 16MHz / (2 * 1024 * (194 + 1) )
+	OCR0A = 194;		//40 = 16MHz / (2 * 1024 * (194 + 1) )*/
 }
 
 ISR(TIMER0_COMPA_vect){
+	
+	PB0_LED_TOGGLE;
+
+	/*
 	static uint8_t countA = 0;
 	static uint8_t countB = 0;
 	
 	countA++;
 	countB++;
-	countForRandomSeed++;
-	
-	if(countForRandomSeed >= 65535){
-		countForRandomSeed = 0;
-	}
 
-	/* make 40Hz into 20Hz */
+	/* make 40Hz into 20Hz *//*
 	if(countA >= 2){
 		countA = 0;
 		if(lightLost){
@@ -136,7 +155,7 @@ ISR(TIMER0_COMPA_vect){
 		}
 	}
 
-	/* make 40Hz into 5Hz */
+	/* make 40Hz into 5Hz *//*
 	if(countB >= 4){
 		countB = 0;
 		lightCorrectNum[0] ? PB0_LED_TOGGLE : PB0_LED_OFF;
@@ -144,7 +163,7 @@ ISR(TIMER0_COMPA_vect){
 		lightCorrectNum[0] ? PB2_LED_TOGGLE : PB2_LED_OFF;
 		lightCorrectNum[0] ? PB3_LED_TOGGLE : PB3_LED_OFF;
 
-	}
+	}*/
 }
 
 /* Setup Timer 1 with a frequency of 1 Hz */
@@ -155,6 +174,9 @@ void init_Timer1()
 
 	// - prescaler 256
 	SetBit(TCCR1B, CS02);
+
+	// enable OCR0A on timer 1
+	SetBit(TIMSK1, OCIE1A);
 	
 	// Set output compare value for channel A
 	OCR1A =  31249;		// 1 = 16MHz / ( 2 * 256 * (31249 + 1) )
@@ -167,19 +189,18 @@ ISR(TIMER1_COMPA_vect){
 }
 
 void init(){
+	// initialise UART
+	InitUART(MYUBRR);
+	
 	// set PB0-5 to output & PB6-7 as input
 	DDRB = 0b00111111;
 
 	// set interupt on Port B
 	PCMSK0 |= (1<<DDB7);
 	PCICR |= (1<<PCIE0);
-
 	// initialise the times
 	init_Timer1();
 	init_Timer0();
-
-	// initialise UART
-	InitUART(MYUBRR);
 }
 
 ISR(PCINT0_vect){
@@ -203,42 +224,13 @@ void setup_New_Game(){
 	PB3_LED_OFF;
 	PB2_LED_OFF;
 	PB1_LED_OFF;
-}
-
-/* Works up to 9999 */
-const char * to_ASCII(int i){
-	char returnValue[4] = {"\0\0\0\0"};
-	if (i % 1000){
-		returnValue[0] = (i/1000) + 48;
-		i = i % 1000;
-		returnValue[1] = (i/100) + 48;
-		i = i % 100;
-		returnValue[2]= (i/10) + 48;
-		i = i % 10;
-		returnValue[3] = i + 48;
-	} else if(i % 100){
-		returnValue[0] = (i/100) + 48;
-		i = i % 100;
-		returnValue[1] = (i/10) + 48;
-		i = i % 10;
-		returnValue[2]= i + 48;
-	} else if (i % 10){
-		returnValue[0] = (i/10) + 48;
-		i = i % 10;
-		returnValue[1]= i + 48;
-	} else {
-	returnValue[0]= i + 48;
-	}
-	return returnValue;
+	PB0_LED_OFF;
 }
 
 void DEBUG_PRINT_STATE(uint8_t state){
 	const char stateText[10][20] = {"welcome", "newGame", "turnX", "askInput", "checkInput", "showResults", "won", "lost", "waiting"};
+	TransmitString("--------- ");
 	TransmitString(stateText[state]);
-	TransmitString(" ----- ");
-	TransmitByte(turn+48);
-	TransmitString(" ----- ");
-	TransmitString(countForRandomSeed);
 	TransmitString("\r\n");
 }
 
@@ -253,15 +245,14 @@ int main(void)
 	sei();
 	currentGameState = welcome;
 
-	_delay_ms(1000);
-
     while (1) 
     {
 		// If the reset flag is set the gamestate will finish 
         if(resetGame) { 
 			currentGameState = newGame;
+			resetGame = 0;
 		}
-		//DEBUG_PRINT_STATE(currentGameState);
+		//DEBUG_PRINT_STATE(currentGameState);			//DEBUG
 		switch(currentGameState){
 
 		/*		Welcome text for the start of the game		*/
@@ -270,13 +261,18 @@ int main(void)
 		TransmitString("Welcome to the awesome game MASTERMIND!\r\n");
 		// This is so a random amount of time has passed for the generation of numbers
 		TransmitString("You will send you guess using the terminal. Try it!\r\n");
-		ReceiveByte();
+		while (ReceiveByte() != '\n'){
+			;
+		}
 		init_Random_Number();
 		TransmitString("Good job!\r\n");
-		_delay_ms(1000);
-		TransmitString("You will have to guess the correct combination of 4 numbers between 1 and 6.\r\n");
-		TransmitString("You will have ");
-		TransmitString("12");//to_ASCII(MAX_TURNS));
+		_delay_ms(500);
+		TransmitString("You will have to guess the correct combination of ");
+		itoa(MM_DIGITS, buffer, 10);
+		TransmitString(buffer);
+		TransmitString(" numbers between 1 and 6.\nYou will have ");
+		itoa(MAX_TURNS, buffer, 10);
+		TransmitString(buffer);
 		TransmitString(" turns to find the correct combination.\r\n");
 		currentGameState = newGame;
 		break;
@@ -289,57 +285,65 @@ int main(void)
 			secret_code[i] = generate_Random_Number();
 		}
 		set_secret_code(secret_code);
-		TransmitString(secret_code);
-		TransmitString(" is the secret code.\r\n"); // DEBUG
+
+		// Cheat mode
+		if (showSecretCode){
+			for (uint8_t i = 0; i < MM_DIGITS; i++){
+				buffer[i] = secret_code[i];
+			}
+			buffer[MM_DIGITS + 1] = '\0';
+			TransmitString(buffer);
+			TransmitString(" is the secret code.\r\n");
+		}
+		
 		currentGameState = turnX;
 		break;
 
 		/*			Check if the max turns have been passed				*/
 		case turnX:
-		if(turn > MAX_TURNS){
+		if(turn >= MAX_TURNS){
 			currentGameState = lost;
 		} else {
 			currentGameState = askInput;
+			turn++;
 		}
 		break;
 
 		/*			Ask for input			*/
 		case askInput:
 		TransmitString("You have reached turn ");
-		TransmitByte(turn + 1 + 48);//to_ASCII(turn));
+		itoa(turn, buffer, 10);
+		TransmitString(buffer);
 		TransmitString(".\r\n");
-		char buffer[30];
-		uint8_t askingForInput = 1;			// TEMP SHOULD BE 1
-		/*buffer[0] = 1;
-		buffer[1] = 1;
-		buffer[2] = 1;
-		buffer[3] = 1;*/
+		uint8_t askingForInput = 1;
 		mm_result.correct_num = 0;
 		mm_result.correct_num_and_pos = 0;
 		// ask for input until the input is between 1 and 6
 		while(askingForInput){
 			// reset the buffer
-			for(uint8_t i = 0; i >= 10; i++){
+			for(uint8_t i = 0; i < BUFFER_SIZE; i++){
 				buffer[i] = '\0';
 			}
+			
 			// ask for numbers
-			TransmitString("Please enter your guess (1-6) like this: '1234'\r\n");
-			_delay_ms(500);
+			TransmitString("Please enter your guess like this: '1234'\r\n");
+			_delay_ms(100);
 			ReceiveString(buffer);
-			TransmitString(buffer); // DEBUG
+
 			// check if the input is correct
-			for(uint8_t i = 0; i > MM_DIGITS; i++){
-				if (buffer[i] >= 1+48 && buffer[i] <= 6+48){
+			for(uint8_t i = 0; i < MM_DIGITS; i++){
+				if (buffer[i] >= '1' && buffer[i] <= '6'){
 					askingForInput = 0;
-					TransmitString("PING!\r\n"); // DEBUG
-					} else {
-					TransmitString("Your guess has to be between 1 and 6, try again.");
+				} else {
+					TransmitString("Your guess has to be between 1 and 6, try again.\r\n");
+
 					askingForInput = 1;
 					break;
 				}
 			}
 		}
-		for(uint8_t i = 0; i > MM_DIGITS; i++){
+
+		for(uint8_t i = 0; i < MM_DIGITS; i++){
 			user_code[i] = buffer[i];
 		}
 		currentGameState = checkInput;
@@ -351,6 +355,10 @@ int main(void)
 		lightCorrectNum[1] = 0;
 		lightCorrectNum[2] = 0;
 		lightCorrectNum[3] = 0;
+		PB0_LED_OFF;
+		PB1_LED_OFF;
+		PB2_LED_OFF;
+		PB3_LED_OFF;
 		// Check the user code against the secret code
 		mm_result = check_secret_code(user_code);
 		if(mm_result.correct_num_and_pos >= 4){
@@ -362,8 +370,8 @@ int main(void)
 
 		/*		Show results to player		 */
 		case showResults:
-		for(uint8_t i = 0; i > MM_DIGITS; i++){
-			if(mm_result.correct_num_and_pos >= i){
+		for(uint8_t i = 0; i < MM_DIGITS; i++){
+			if(mm_result.correct_num_and_pos > i){
 				switch(i){
 				case 0:
 				PB0_LED_ON;
@@ -381,19 +389,20 @@ int main(void)
 				break;
 				}
 			} else {
-				if(mm_result.correct_num >= i + mm_result.correct_num_and_pos){
+				if(mm_result.correct_num > i - mm_result.correct_num_and_pos){
 					lightCorrectNum[i] = 1;
 				}
 			}
 		}
 
 		TransmitString("You have guessed ");
-		TransmitString(mm_result.correct_num_and_pos + 48);
+		itoa(mm_result.correct_num_and_pos, buffer, 10);
+		TransmitString(buffer);
 		TransmitString(" with the correct number at the correct position.\r\n");
 		TransmitString("You have guessed ");
-		TransmitString(mm_result.correct_num + 48);
+		itoa(mm_result.correct_num, buffer, 10);
+		TransmitString(buffer);
 		TransmitString(" with the correct number.\r\n");
-		turn++;
 		currentGameState = turnX;
 		break;
 
@@ -493,7 +502,12 @@ char ReceiveByte(void)
     unsigned char tmptail;
     
     /* Wait for incoming data */
-    while (UART_RxHead == UART_RxTail);
+    while (UART_RxHead == UART_RxTail){
+	    countForRandomSeed += 1;
+	    if(countForRandomSeed > 65534){
+		    countForRandomSeed = 0;
+	    }
+    }
     
     /* Calculate buffer index */
     tmptail = (UART_RxTail + 1) & UART_RX_BUFFER_MASK;
